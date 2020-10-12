430128a240c99fa27b9ba212888fb594bd52ccff
Janfix <jean.philippe.riviere@gmail.com>
2020-10-12 16:09:55 +0200

    fix dashboard orthographe

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -20,7 +20,7 @@
                     $widgets['before_content'][] = [
                         'type'        => 'jumbotron',
                         'heading'     => trans('correction.Welcome in the correction module'),
-                        'content'     => "Bienvenue dans le module de correction. Vous avez été inscrit en tant que correcteur. Trois types de corrections sont désormains disponible sur cette plateforme : 1.Evaluer la fluence en lecture de texte, 2. Evaluer la production orale, 3. Evaluer la lecture orale de mots isolés. En cliquant sur le lien ci-contre : \"Liste de correction\", vous allez pouvoir accéder aux corrections qui vous auront été attribuées. ",
+                        'content'     => "Bienvenue dans le module de correction. Vous avez été inscrit(e) en tant que correcteur. Trois types de corrections sont désormains disponibles sur cette plateforme : 1.Evaluer la fluence en lecture de texte, 2. Evaluer la production orale, 3. Evaluer la lecture orale de mots isolés. En cliquant sur le lien ci-contre : \"Liste de correction\", vous allez pouvoir accéder aux corrections qui vous auront été attribuées. ",
                         'button_link' => backpack_url('logout'),
                         'button_text' => trans('backpack::base.logout'),
                     ];

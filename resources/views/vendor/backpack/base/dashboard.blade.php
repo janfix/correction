3045c8118ef0ecf5f5cbd30b8a4f08176d9f43d8
Janfix <jean.philippe.riviere@gmail.com>
2020-10-12 16:00:35 +0200

    Jumbo profil def.

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,19 +1,20 @@
 @extends(backpack_view('blank'))
 
-@php
-    $widgets['before_content'][] = [
-        'type'        => 'jumbotron',
-        'heading'     => trans('correction.Welcome in the correction module'),
-        'content'     => trans('correction.explication'),
-        'button_link' => backpack_url('logout'),
-        'button_text' => trans('backpack::base.logout'),
-    ];
-@endphp
+
 
 @section('content')
 
                 @if (auth()->user()->isAdministrator())
                     Hello Admin
+                    @php
+                    $widgets['before_content'][] = [
+                        'type'        => 'jumbotron',
+                        'heading'     => trans('correction.Welcome in the correction module'),
+                        'content'     => trans('correction.explication'),
+                        'button_link' => backpack_url('logout'),
+                        'button_text' => trans('backpack::base.logout'),
+                    ];
+                    @endphp
                 @elseif(auth()->user()->isCorrector())
                     Hello Corrector
                 @else
